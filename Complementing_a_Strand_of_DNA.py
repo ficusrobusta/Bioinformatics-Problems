3cd92c6eecee5232ef47fad7ce3911a0add7ed1b
ficusrobusta <70322151+ficusrobusta@users.noreply.github.com>
2021-01-05 11:57:51 +0000

    Update Complementing_a_Strand_of_DNA.py

--- Complementing_a_Strand_of_DNA.py
+++ Complementing_a_Strand_of_DNA.py
@@ -1,19 +1,19 @@
-The Secondary and Tertiary Structures of DNA
+# The Secondary and Tertiary Structures of DNA
 
-Problem
+# Problem
 
-In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
-The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
-Given: A DNA string s of length at most 1000 bp.
-Return: The reverse complement sc of s.
+# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+# The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+# Given: A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
 
-Sample Dataset
-AAAACCCGGT
+# Sample Dataset
+# AAAACCCGGT
 
-Sample Output
-ACCGGGTTTT
+# Sample Output
+# ACCGGGTTTT
 
-Solution
+# Solution
 
 DNA = "AAAACCCGGT"
 
